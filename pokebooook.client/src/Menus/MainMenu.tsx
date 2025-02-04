d9d0bf41fcad750ba723201a135230a5a5867634
ChlebaDev <filous.bradac@gmail.com>
2025-02-04 23:10:13 +0100

    backgrounds to webp

--- pokebooook.client/src/Menus/MainMenu.tsx
+++ pokebooook.client/src/Menus/MainMenu.tsx
@@ -4,26 +4,26 @@ import MenuBtn from "../components/MenuBtn";
 import Bg from "../components/Bg";
 
 const MainMenu: React.FC = () => {
-  return (
-    <>
-      <Bg imageId={153}>
-        <div className={MainMenuCSS.mainMenu__buttons__container}>
-          <Link to={"/nickname"}>
-            <MenuBtn btnText="Hrát" />
-          </Link>
-          <Link to={"/statistics"}>
-            <MenuBtn btnText="Statistiky" />
-          </Link>
-        </div>
+    return (
+        <>
+            <Bg imageId={300}>
+                <div className={MainMenuCSS.mainMenu__buttons__container}>
+                    <Link to={"/nickname"}>
+                        <MenuBtn btnText="Hrát" />
+                    </Link>
+                    <Link to={"/statistics"}>
+                        <MenuBtn btnText="Statistiky" />
+                    </Link>
+                </div>
 
-        <Link to={"/admin"}>
-          <p className={MainMenuCSS.mainMenu__adminlink}>
-            <b>Administrace</b>
-          </p>
-        </Link>
-      </Bg>
-    </>
-  );
+                <Link to={"/admin"}>
+                    <p className={MainMenuCSS.mainMenu__adminlink}>
+                        <b>Administrace</b>
+                    </p>
+                </Link>
+            </Bg>
+        </>
+    );
 };
 
 export default MainMenu;
