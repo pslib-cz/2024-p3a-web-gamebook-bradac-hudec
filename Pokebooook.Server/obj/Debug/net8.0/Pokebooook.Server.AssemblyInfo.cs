ab1c9ba726845342989febd14ba851921d542453
Filip Hudec <filip.hudec.022@pslib.cz>
2025-03-29 21:53:49 +0100

    commit mozna zacharna

--- Pokebooook.Server/obj/Debug/net8.0/Pokebooook.Server.AssemblyInfo.cs
+++ Pokebooook.Server/obj/Debug/net8.0/Pokebooook.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokebooook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a672a3cd5e7076b3039a2027f31c4ae1735244d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c0bd37332e5dcd5bf0b3f31dd07d7e1a0962b3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokebooook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokebooook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
