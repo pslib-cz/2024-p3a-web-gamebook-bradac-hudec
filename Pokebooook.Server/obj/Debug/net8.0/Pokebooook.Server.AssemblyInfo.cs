2f0e5b38c50a71c0791656821c571102f2ed6fa1
Filip Hudec <filip.hudec.022@pslib.cz>
2025-03-29 22:54:59 +0100

    zmeny

--- Pokebooook.Server/obj/Debug/net8.0/Pokebooook.Server.AssemblyInfo.cs
+++ Pokebooook.Server/obj/Debug/net8.0/Pokebooook.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokebooook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f727e365631bdf24182b9625972943e82ee6d8d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b59a9020b80a4dcecdb3212cdaa7dfe006da492c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokebooook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokebooook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
